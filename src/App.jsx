442a5d9989d143bad4bc65aab513acace19d8e5b
Salman Hamdouchi <th3salman@gmail.com>
2023-07-30 14:37:27 +0100

    Removed font awesome and added a new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
+import { Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Dashboard from "./components/Dashboard";
@@ -15,14 +11,14 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={Dashboard} />
-        <Route path="/conversation" element={Conversation} />
-        <Route path="/video-generation" element={VideoGeneration} />
-        <Route path="/image-generation" element={ImageGeneration} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/conversation" element={<Conversation />} />
+        <Route path="/video-generation" element={<VideoGeneration />} />
+        <Route path="/image-generation" element={<ImageGeneration />} />
       </Routes>
     </Layout>
   );
 }
 
 export default App;
-library.add(fas, far);
