ad1d3c33946ef373bc4356d9107ba900a137ca88
Salman Hamdouchi <th3salman@gmail.com>
2023-07-30 14:49:58 +0100

    Made sidebar items more dynamic and added the logic for closing the sidebar on moile devices

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,74 +1,78 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-const SideBar = () => {
+import { Link, useLocation } from "react-router-dom";
+import {
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  VideoIcon,
+} from "lucide-react";
+const SideBar = ({ isSideBarOpen, setIsSideBarOpen }) => {
+  const location = useLocation();
+  const pathname = location.pathname;
+  const links = [
+    {
+      to: "/dashboard",
+      icon: LayoutDashboard,
+      color: "#0284c7",
+      label: "Dashboard",
+    },
+    {
+      to: "/conversation",
+      icon: MessageSquare,
+      color: "#f43f5e",
+      label: "Conversation",
+    },
+    {
+      to: "/image-generation",
+      icon: ImageIcon,
+      color: "#14b8a6",
+      label: "Image generation",
+    },
+    {
+      to: "/video-generation",
+      icon: VideoIcon,
+      color: "#f97316",
+      label: "Video generation",
+    },
+  ];
   return (
-    <div className="h-[100vh] md:flex md:w-72 md:flex-col md:sticky md:inset-y-0 z-[100] bg-gray-900 sm:hidden">
-      <div className="space-y-4 py4 flex flex-col h-full bg-[#111827] text-white">
-        <div className="px-3 py-2 flex-1">
-          <div className="flex items-center justify-center mt-4 mb-8">
-            <img className="w-[40px] mr-1" src="/logo.png" alt="logo"></img>
-            <h1 className="text-2xl font-semibold ">Clever</h1>
-          </div>
+    <>
+      {isSideBarOpen && (
+        <div
+          className="fixed inset-0 bg-black opacity-20 z-50"
+          onClick={() => setIsSideBarOpen(false)}
+        />
+      )}
+      <div
+        className={` ${
+          !isSideBarOpen ? "xsm:-translate-x-96 xsm:hidden" : ""
+        } h-[100vh] md:flex md:w-96 md:flex-col fixed md:inset-y-0 z-[100] bg-gray-900 md:translate-x-0`}>
+        <div className="space-y-4 py4 flex flex-col h-full bg-[#111827] text-white z-[200]">
+          <div className="px-3 py-2 flex-1">
+            <div className="flex items-center justify-center mt-4 mb-8">
+              <img className="w-[60px] mr-1" src="/logo.png" alt="logo"></img>
+              <h1 className="text-4xl font-semibold ">Clever</h1>
+            </div>
 
-          <div className="pl-4 pr-4">
-            <Link to="/dashboard">
-              <div className="pt-4 pb-4 pl-2 pr-2 flex items-center rounded-lg hover:bg-[#2d3340] cursor-pointer transition-all duration-300">
-                <FontAwesomeIcon
-                  icon="fa-solid fa-layer-group "
-                  size="xl"
-                  className="mr-3"
-                  color="#0284c7"
-                />
-                <h3 className="text-white text-[1.15rem] font-light">
-                  Dashboard
-                </h3>
-              </div>
-            </Link>
-            <Link to="/conversation">
-              <div className="pt-4 pb-4 pl-2 pr-2 flex items-center rounded-lg hover:bg-[#2d3340] cursor-pointer transition-all duration-300">
-                <FontAwesomeIcon
-                  icon="fa-solid fa-message"
-                  size="xl"
-                  className="mr-3"
-                  color="#f43f5e"
-                />
-                <h3 className="text-white text-[1.15rem] font-light">
-                  Conversation
-                </h3>
-              </div>
-            </Link>
-            <Link to="/image-generation">
-              <div className="pt-4 pb-4 pl-2 pr-2 flex items-center rounded-lg hover:bg-[#2d3340] cursor-pointer transition-all duration-300">
-                <FontAwesomeIcon
-                  icon="fa-solid fa-image"
-                  size="xl"
-                  className="mr-3"
-                  color="#14b8a6"
-                />
-                <h3 className="text-white text-[1.15rem] font-light">
-                  Image generation
-                </h3>
-              </div>
-            </Link>
-            <Link to="video-generation">
-              <div className="pt-4 pb-4 pl-2 pr-2 flex items-center rounded-lg hover:bg-[#2d3340] cursor-pointer transition-all duration-300">
-                <FontAwesomeIcon
-                  icon="fa-solid fa-video"
-                  size="xl"
-                  className="mr-3"
-                  color="#f97316"
-                />
-                <h3 className="text-white text-[1.15rem] font-light rounded-lg hover:bg-[#2d3340] cursor-pointer transition-all duration-300">
-                  Video generation
-                </h3>
-              </div>
-            </Link>
+            <div className="pl-4 pr-4">
+              {links.map((link, index) => (
+                <Link key={index} to={link.to}>
+                  <div
+                    className={`${
+                      pathname === link.to ? "bg-[#2d3340d2]" : ""
+                    } pt-4 pb-4 pl-2 pr-2 flex items-center rounded-lg hover:bg-[#2d3340] cursor-pointer transition-all duration-300`}>
+                    <link.icon className="mr-3" size="25" color={link.color} />
+                    <h3 className="text-white text-[1.15rem] font-light">
+                      {link.label}
+                    </h3>
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
