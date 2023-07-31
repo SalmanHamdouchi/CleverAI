972147814e3cdd3da0025a90d5ed0500465dffc3
Salman Hamdouchi <th3salman@gmail.com>
2023-07-31 22:03:53 +0100

    Added CORS config

--- backend/config.js
+++ backend/config.js
@@ -7,6 +7,19 @@ const config = {
   openai: {
     apiKey: OPENAI_API_KEY,
   },
+  allowedOrigins: ["http://localhost:3000"],
 };
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Check if the request origin is in the allowedOrigins array or if it is undefined (e.g., for non-browser requests)
+    if (!origin || config.allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+export { corsOptions };
 export default config;
