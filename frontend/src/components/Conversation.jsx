dbca285993f1551883582b4542eae99a0d717eba
Salman Hamdouchi <th3salman@gmail.com>
2023-07-31 22:11:33 +0100

    Updated the conversation component to use the conversation slice

--- frontend/src/components/Conversation.jsx
+++ frontend/src/components/Conversation.jsx
@@ -1,7 +1,22 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectAllMessages,
+  addMessage,
+  generateTextThunk,
+  selectLoading,
+} from "../features/conversation/conversation-slice";
 import { MessageSquare } from "lucide-react";
+import toast, { Toaster } from "react-hot-toast";
+import Empty from "./Empty";
+import Loader from "./Loader";
+import Avatar from "./Avatar";
 
 const Conversation = () => {
+  let dispatch = useDispatch();
+  let messages = useSelector(selectAllMessages);
+  let isLoading = useSelector(selectLoading);
+
   const [formData, setFormData] = useState({
     prompt: "",
   });
@@ -16,17 +31,24 @@ const Conversation = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const validationErrors = {};
 
-    if (!formData.prompt.trim()) {
-      validationErrors.prompt = "Prompt is required";
-    } else {
-      console.log(formData);
+    if (!formData.prompt || !formData.prompt.trim()) {
+      toast.error("Please fill in the prompt!");
+      return;
     }
+
+    dispatch(generateTextThunk(formData.prompt)).then((data) => {
+      if (!data.error) {
+        dispatch(addMessage(formData.prompt));
+      } else {
+        toast.error("Something went wrong, please try again!");
+      }
+    });
   };
 
   return (
     <div className="flex flex-col md:px-10">
+      <Toaster />
       <div className="flex items-center mb-10">
         <div
           className={`h-16 w-16 flex items-center justify-center rounded-md mr-3 text-violet-500 bg-violet-500/10`}>
@@ -48,14 +70,38 @@ const Conversation = () => {
           onChange={handleInputChange}
           placeholder="Explain DevOps to me like I'm 5 years old"
           className="resize-none w-full xl:grow-1 min-h-5 border-0 outline-none focus-visible:ring focus-visible:ring-transparent xl:pr-8 sm:pb-8 xl:pb-0 sm:pr-0 overflow-hidden overflow-y-auto"></textarea>
-        {/* {errors.prompt && <p>{errors.prompt}</p>} */}
         <button
           className="w-full xl:w-28 rounded-md py-2 bg-[#121725] text-white font-semibold"
           type="submit">
           Generate
         </button>
       </form>
-      <div className="space-y-4 mt-4">Messages Content</div>
+      <div className="space-y-4 mt-4">
+        {isLoading && (
+          <div className="p-8 rounded-lg w-full flex justify-center items-center bg-[#EFF3F8]">
+            <Loader />
+          </div>
+        )}
+        {messages.length === 0 && !isLoading && (
+          <Empty label="No Conversation Started!" />
+        )}
+        <div className="flex flex-col-reverse gap-y-4">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`py-6 px-8 w-full flex gap-x-8 border rounded-lg items-center border-black/10 ${
+                message.role === "user" ? "bg-white" : "bg-[#EFF3F8]"
+              }`}>
+              {message.role === "user" ? (
+                <Avatar imageURL="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" />
+              ) : (
+                <Avatar imageURL="/logo.png" />
+              )}
+              <p> {message.content}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
