2acc1e71800ca84fec19d9de15adb6556f866a41
Salman Hamdouchi <th3salman@gmail.com>
2023-07-31 22:04:19 +0100

    Applied CORS on express

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
 import apiRoutes from "./routes/api-routes.js";
+import corseOptions from "./config.js";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors(corseOptions));
 app.use("/api", apiRoutes);
 
 app.use((err, req, res, next) => {
