259eed60e7fa39a8a4c7873488b197b8179fd2f6
Salman Hamdouchi <th3salman@gmail.com>
2023-07-31 13:14:44 +0100

    Created the server configuration withe express

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import apiRoutes from "./routes/api-routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api", apiRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: err });
+});
+
+// Start the server
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
