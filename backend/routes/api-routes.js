d7bf46f30e8c0e8ced91dd0ca9b8f4111a3d66f9
Salman Hamdouchi <th3salman@gmail.com>
2023-08-01 19:24:38 +0100

    Add routes

--- backend/routes/api-routes.js
+++ backend/routes/api-routes.js
@@ -1,8 +1,15 @@
 import express from "express";
 import TextController from "../controllers/text-controller.js";
+import CodeController from "../controllers/code-controller.js";
+import ImageController from "../controllers/image-controller.js";
 
 let textController = new TextController();
+let codeController = new CodeController();
+let imageController = new ImageController();
+
 const router = express.Router();
 router.post("/generate-text", textController.generateText);
+router.post("/generate-code", codeController.generateCode);
+router.post("/generate-image", imageController.generateImage);
 
 export default router;
