5735fec3d4ab6e559dbfe92a4f774f22c958355c
Salman Hamdouchi <th3salman@gmail.com>
2023-08-01 19:25:32 +0100

    Add code and image reducer to Redux store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import conversationReducer from "../features/conversation/conversation-slice";
+import codeReducer from "../features/code/code-slice";
 import sidebarReducer from "../features/ui/sidebar-slice";
+import imageSlice from "../features/image/image-slice";
 
 const store = configureStore({
   reducer: {
     conversation: conversationReducer,
+    code: codeReducer,
+    image: imageSlice,
     sidebar: sidebarReducer,
   },
 });
