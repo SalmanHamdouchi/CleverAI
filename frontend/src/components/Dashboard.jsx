ba0f59138eb65322708901f1df011455e6287049
Salman Hamdouchi <th3salman@gmail.com>
2023-08-01 19:26:39 +0100

    Add code generation button to Dashboard and make some modifications

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -19,6 +19,13 @@ const Dashboard = () => {
       bgColor: "bg-violet-500/10",
     },
     {
+      label: "Code",
+      icon: Code,
+      href: "/code-generation",
+      color: "text-yellow-500",
+      bgColor: "bg-yellow-500/10",
+    },
+    {
       label: "Music Generation",
       icon: Music,
       href: "/music",
@@ -30,21 +37,21 @@ const Dashboard = () => {
       icon: ImageIcon,
       color: "text-pink-700",
       bgColor: "bg-pink-700/10",
-      href: "/image",
+      href: "/image-generation",
     },
     {
       label: "Video Generation",
       icon: VideoIcon,
       color: "text-orange-700",
       bgColor: "bg-orange-700/10",
-      href: "/video",
+      href: "/video-generation",
     },
     {
       label: "Code Generation",
       icon: Code,
       color: "text-green-700",
       bgColor: "bg-green-700/10",
-      href: "/code",
+      href: "/code-generation",
     },
   ];
   return (
@@ -61,10 +68,8 @@ const Dashboard = () => {
 
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((item, index) => (
-          <Link to={item.href}>
-            <div
-              key={index}
-              className="p-4 rounded-lg border-2 border-black/5 flex items-center justify-between mb-4 hover:shadow-[0px_2px_3px_0px_#71717112] transition cursor-pointer">
+          <Link key={index} to={item.href}>
+            <div className="p-4 rounded-lg border-2 border-black/5 flex items-center justify-between mb-4 hover:shadow-[0px_2px_3px_0px_#71717112] transition cursor-pointer">
               <div className={`flex items-center p-2 rounded-lg`}>
                 <div
                   className={`h-10 w-10 flex items-center justify-center rounded-md ${item.bgColor} ${item.color}`}>
