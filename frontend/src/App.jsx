cc1c5dd31190f258450ce15ad8ec9efcfea28f4e
Salman Hamdouchi <th3salman@gmail.com>
2023-08-01 19:25:12 +0100

    Add code generation route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Dashboard from "./components/Dashboard";
 import Conversation from "./components/Conversation";
 import VideoGeneration from "./components/VideoGeneration";
 import ImageGeneration from "./components/ImageGeneration";
+import CodeGeneration from "./components/CodeGeneration";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/conversation" element={<Conversation />} />
+        <Route path="/code-generation" element={<CodeGeneration />} />
         <Route path="/video-generation" element={<VideoGeneration />} />
         <Route path="/image-generation" element={<ImageGeneration />} />
       </Routes>
