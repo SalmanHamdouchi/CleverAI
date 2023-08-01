b4d4a77ede7e753238a40a797b1fe17d26ede8fe
Salman Hamdouchi <th3salman@gmail.com>
2023-08-01 19:23:53 +0100

    Modify text generation and add image generation

--- backend/services/openai-service.js
+++ backend/services/openai-service.js
@@ -9,14 +9,36 @@ class OpenaiService {
     this.openai = new OpenAIApi(configuration);
   }
 
-  async generateText(prompt) {
+  async generateText(instruction, prompt) {
+    let messages =
+      instruction != null
+        ? [
+            { role: "system", content: instruction },
+            { role: "user", content: prompt },
+          ]
+        : [{ role: "user", content: prompt }];
     try {
       const response = await this.openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: prompt }],
-        max_tokens: 30,
+        messages: messages,
+        max_tokens: 40,
       });
       return response.data.choices[0].message.content;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async generateImage(prompt) {
+    console.log(prompt);
+    try {
+      const response = await this.openai.createImage({
+        prompt,
+        n: 1,
+        size: "256x256",
+      });
+      console.log(response.data);
+      return response.data.data[0].url;
     } catch (error) {
       throw new Error(error.message);
     }
