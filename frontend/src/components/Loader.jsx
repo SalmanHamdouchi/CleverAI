119b2cc66662a31116e0687a18d1e2268fffef11
Salman Hamdouchi <th3salman@gmail.com>
2023-08-05 23:38:21 +0100

    Add an optional information to the loader component

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Loader = () => {
+const Loader = ({ information }) => {
   return (
     <div className="h-full flex flex-col gap-y-4 items-center justify-center ">
       <div className="w-14 h-14 relative animate-spin">
         <img src="/logo.png" alt="logo" />
       </div>
-      <p className="text-md">Clever is thinking...</p>
+      <p className="text-lg">Clever is thinking...</p>
+      {information && <p className="text-sm">{information}</p>}
     </div>
   );
 };
