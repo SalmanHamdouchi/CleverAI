f7dce2e6b11748c5fb2da035b8d5337e32465f2c
Salman Hamdouchi <th3salman@gmail.com>
2023-08-05 23:42:45 +0100

    Update backend API routes

--- backend/routes/api-routes.js
+++ backend/routes/api-routes.js
@@ -2,14 +2,21 @@ import express from "express";
 import TextController from "../controllers/text-controller.js";
 import CodeController from "../controllers/code-controller.js";
 import ImageController from "../controllers/image-controller.js";
+import VideoController from "../controllers/video-controller.js";
 
 let textController = new TextController();
 let codeController = new CodeController();
 let imageController = new ImageController();
+let videoController = new VideoController();
 
 const router = express.Router();
 router.post("/generate-text", textController.generateText);
 router.post("/generate-code", codeController.generateCode);
 router.post("/generate-image", imageController.generateImage);
+router.post(
+  "/generate-image-replicate",
+  imageController.generateImageReplicate
+);
+router.post("/generate-video", videoController.generateVideo);
 
 export default router;
