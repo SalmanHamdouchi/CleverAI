96e09e6a12e0441e43d26219994f43503685d48f
Salman Hamdouchi <th3salman@gmail.com>
2023-09-20 15:41:40 +0100

    Converted the service module into typescript

---
+++ backend/src/services/openaiService.ts
@@ -0,0 +1,47 @@
+import config from "../config.js";
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from "openai";
+
+class OpenaiService {
+  private openAiApi!: OpenAIApi;
+
+  constructor() {
+    this.initOpenApi();
+  }
+
+  initOpenApi = (): void => {
+    const configuration = new Configuration({
+      apiKey: config.openai.apiKey,
+    });
+    if (config.openai.apiKey !== undefined) {
+      this.openAiApi = new OpenAIApi(configuration);
+    } else {
+      throw new Error("Replicate API token is undefined");
+    }
+  };
+
+  async generateText(
+    instruction: string,
+    prompt: string
+  ): Promise<string | Error> {
+    let conversation: ChatCompletionRequestMessage[] =
+      instruction != null
+        ? [
+            { role: "system", content: instruction },
+            { role: "user", content: prompt },
+          ]
+        : [{ role: "user", content: prompt }];
+    try {
+      const response = await this.openAiApi.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: conversation,
+        max_tokens: 200,
+      });
+      return response.data.choices[0].message?.content || "Undefined";
+    } catch (error) {
+      if (error instanceof Error) return new Error(error.message);
+      return new Error("Failed to call OpenAI's API");
+    }
+  }
+}
+
+export default OpenaiService;
