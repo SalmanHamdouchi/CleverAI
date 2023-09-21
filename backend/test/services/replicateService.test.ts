a88317ac0fc95e6b857f8b4ae7c31ff723ca1117
Salman Hamdouchi <th3salman@gmail.com>
2023-09-21 20:43:56 +0100

    remove test skipping

--- backend/test/services/replicateService.test.ts
+++ backend/test/services/replicateService.test.ts
@@ -7,17 +7,17 @@ describe("ReplicateService", () => {
     replicateService = new ReplicateService();
   });
 
-  it.skip("should initialize Replicate API", () => {
+  it("should initialize Replicate API", () => {
     expect(replicateService["replicate"]).toBeDefined();
   });
 
-  it.skip("should generate an image", async () => {
+  it("should generate an image", async () => {
     const prompt = "Generate an image";
     const output = await replicateService.generateImage(prompt);
     expect(output).toHaveLength(1);
   });
 
-  it.skip("should generate a video", async () => {
+  it("should generate a video", async () => {
     const prompt = "Generate a video";
     const output = await replicateService.generateVideo(prompt);
     expect(output).toHaveLength(1);
